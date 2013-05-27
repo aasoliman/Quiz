23d802f2b6a5b2308c8356da6936af988ad6121f
Abdulaziz Alsoliman <a.alsoliman@gmail.com>
2013-05-27 14:32:08 -0700

    First Commit

--- src/cgu/edu/ist380/solimana/utorial1/quiz/MainActivity.java
+++ src/cgu/edu/ist380/solimana/utorial1/quiz/MainActivity.java
@@ -1,30 +1,16 @@
-package cgu.edu.ist380.solimana.tutorial.quiz;
+package cgu.edu.ist380.solimana.utorial1.quiz;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private int currentQuestion;
-	private String [] questions;
-	private String [] answers;
-	private Button answerButton;
-	private Button questionButton;
-	private TextView questionView;
-	private TextView answerView;
-	private EditText answerText;
-	
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}	
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
